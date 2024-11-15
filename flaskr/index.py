de56e8ff85870f5659a3bb8a490ebd9756b871d8
Alan <alancastillejos3@gmail.com>
2024-11-15 01:53:44 -0600

    added index

--- flaskr/index.py
+++ flaskr/index.py
@@ -3,4 +3,8 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
-from flaskr.db import get_deb
+bp=Blueprint('index', __name__)
+
+@bp.route('/#', methods=("GET"))
+def index():
+    return render_template('index.html')
