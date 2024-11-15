e360cb260d6190a22910097f2583c27f52569568
Alan <alancastillejos3@gmail.com>
2024-11-15 01:34:45 -0600

    Changed Everything

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask
-
-def cretae_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr')
-    )
